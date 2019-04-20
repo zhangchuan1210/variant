eb421c300822a6d38fb4d3db658d0aec27465e6d
zhangchuan <zhangchuan1210@outlook.com>
2019-04-20 19:28:27 +0800

    tijiaodao

---
+++ variant/variant.cpp
@@ -0,0 +1,16 @@
+﻿// variant.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+template<typename T>
+struct function_traits :public function_traits<decltype(&T::operator())> {};
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
